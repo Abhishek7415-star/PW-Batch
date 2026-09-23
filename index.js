0a98e8833f6c69705def869e9ec4e4363df4090b
React11 <abhishekporwal88978@gmai>
2026-09-23 16:06:14 +0600

    your message

---
+++ index.js
@@ -0,0 +1,15 @@
+let employee={
+    name:"abhi",
+    age:21,
+    gender:"male",
+    blood_group:"A+",
+    bank_details:{
+        Account_number:"1235543",
+        Account_name:"abhi",
+    },
+    Skills:{
+        frontend:"html,css,js",
+        backend:"node,express",
+    }
+}
+console.log(employee);
